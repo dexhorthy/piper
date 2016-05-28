3b2a61e8534aefe3c289e39e2a8d7565e91170f0
Dex <dexter.i.horthy@gmail.com>
2016-05-27 17:25:17 -0700

    use go routines for each pipe

--- piper.go
+++ piper.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+
     "log"
     "flag"
+    "sync"
+	"github.com/marpaia/graphite-golang"
+	"strconv"
+    "database/sql"
+    "fmt"
+
+    _ "github.com/lib/pq"
 )
 
 func main() {
@@ -16,27 +24,54 @@ func main() {
 
     log.Print("Loaded config from ", *config_file)
 
-    for _, pipe := range config.Pipes {
-        executePipe(pipe, config.Source, config.Graphite)
+    Graphite, err := graphite.NewGraphite(config.Graphite.Host, config.Graphite.Port)
+    if err != nil {
+        log.Fatal(err)
     }
+
+    source := config.Source
+
+    db, err := sql.Open("postgres", fmt.Sprintf("user=%v dbname=%v password=%v host=%v port=%v", source.User, source.Database, source.Password, source.Host, source.Port))
+    if err != nil {
+	    log.Fatal(err)
+    }
+
+
+    var wg sync.WaitGroup
+
+    for _, pipe := range config.Pipes {
+        wg.Add(1)
+        go func(pipe PipeConfig) {
+            defer wg.Done()
+            pipe.pipe(db, Graphite)
+        }(pipe)
+    }
+
+    wg.Wait()
 
 }
 
-func executePipe(pipe PipeConfig, dataSource DataSourceConfig, graphite GraphiteConfig) {
+func (pipe *PipeConfig) pipe(db *sql.DB, graphite *graphite.Graphite) {
 
-    log.Print("Executing query: ", pipe.Query)
-    value, err := Extract(dataSource, pipe.Query)
+    rows, err := db.Query(pipe.Query)
 
     if err != nil {
-        log.Fatal(err)
+        log.Print(err)
     }
 
-    log.Print("Loaded value: ", value)
+    result := 0.0
 
-    err = Report(value, pipe.Dest, graphite.Host, graphite.Port)
+    if rows.Next() {
+        rows.Scan(&result)
+    }
+
+
+    err = graphite.SimpleSend(pipe.Dest, strconv.FormatFloat(result, 'E', -1, 64))
+
+    log.Printf("Sent %6.2f \t%v", result, pipe.Dest)
 
     if err != nil {
-        log.Fatal(err)
+        log.Print(err)
     }
 
 }
